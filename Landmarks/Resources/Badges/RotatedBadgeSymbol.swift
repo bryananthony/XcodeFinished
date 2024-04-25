976acd70de9dc0012b25ff2bd3fda4a7b2303d0b
bryananthony <bryananthony872@gmail.com>
2024-04-25 13:27:09 +0700

    Chapter 2

---
+++ Landmarks/Resources/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by MacBook Pro on 25/04/24.
+//  Copyright © 2024 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
